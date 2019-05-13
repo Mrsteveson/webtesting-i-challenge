050ff48f8e302f73d05221721b52613c39060056
mrsteveson <patrick.steveson727@gmail.com>
2019-05-13 14:23:40 -0500

    succeed, fail and repair tests operational

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,46 @@ module.exports = {
   get,
 };
 
+// Successful enhancement
 function succeed(item) {
-  return { ...item };
-}
+  item = {...item, enhancement: item}
+  
+  if(item.enhancement !== 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else {
+    return {...item}
+  }
+};
 
+// Failed enhancement
 function fail(item) {
-  return { ...item };
-}
+  if(item.enhancement < 15) {
+    return {
+      ...item,
+      durability: item.durability - 5
+    };
+  } else {
+    if(item.enhancement === 15) {
+      return {
+        ...item,
+        durability: item.durability - 10
+      }
+    } else {
+      return {
+        ...item,
+        durability: item.durability - 10,
+        enhancement: item.enhancement - 1
+      };
+    };
+  };
+};
 
+// After a repair
 function repair(item) {
-  return { ...item };
-}
+  return { ...item, durability: 100 };
+};
 
+// Grab item
 function get(item) {
   return { ...item };
-}
+};
